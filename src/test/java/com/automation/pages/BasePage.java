7f6b0935f88a6b2d8508edd42fb95565a292bc77
Vishal Vijaya Shankar(UST <291498@ust.com>
2025-05-17 12:47:26 +0530

    Updated TestRunner

--- src/test/java/com/automation/pages/BasePage.java
+++ src/test/java/com/automation/pages/BasePage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class BasePage {
+public abstract class BasePage {
 
     WebDriver driver;
     WebDriverWait wait;
